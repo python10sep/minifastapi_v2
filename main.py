0fd17b803c18a549a777f8750433e45b99264575
python10sep <prashantvelocity123@gmail.com>
2023-03-10 08:26:52 +0530

    update

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+
+class UserPostRequest(BaseModel):
+
+    id: int
+    username: str
+    email: str
+    first_name: str
+
+
+class UserPostResponse(BaseModel):
+    id: int
+    username: str
+    email: str
 
 
 app = FastAPI()
@@ -38,7 +53,7 @@ def get_db():
         db.close()
 
 
-create_sample_user()
+# create_sample_user()
 
 
 @app.get("/")
@@ -55,3 +70,19 @@ def read_all(db: Session = Depends(get_db)):
     """
     return db.query(models.Users).all()
 
+
+@app.post("/", response_model=UserPostResponse)
+def create_user(user_data: UserPostRequest, db: Session = Depends(get_db)):
+    user_model = models.Users()
+    user_model.id = user_data.id
+    user_model.username = user_data.username
+    user_model.email = user_data.email
+    user_model.first_name = user_data.first_name
+    db.add(user_model)
+    db.commit()
+    response = UserPostResponse(
+        id=user_model.id,
+        username=user_model.username,
+        email=user_model.email)
+
+    return response
